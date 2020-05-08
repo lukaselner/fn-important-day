395a4966e71b99e502f768e54e492de199190bb2
Lukáš Elner <65008657+lukaselner@users.noreply.github.com>
2020-05-08 09:12:13 +0200

    Update fn-important-day.php

--- fn-important-day.php
+++ fn-important-day.php
@@ -10,7 +10,7 @@ function importantDay($dayD) {
 	$unixTime = strtotime($dayD);
 	$dayNameText = array('Mon' => 'Pondělí', 'Tue' => 'Úterý', 'Wed' => 'Středa', 'Thu' => 'Čtvrtek', 'Fri' => 'Pátek', 'Sat' => 'Sobota', 'Sun' => 'Neděle');
 	$monthNameText = array('Jan' => 'ledna', 'Feb' => 'února', 'Mar' => 'března', 'Apr' => 'dubna', 'May' => 'května', 'Jun' => 'června', 'Jul' => 'července', 'Aug' => 'srpna', 'Sep' => 'září', 'Oct' => 'října', 'Nov' => 'listopadu', 'Dec' => 'prosince');
-	echo $dayNameText[Date('D', $unixTime)] . ', ' . date('j', $unixTime) . '. ' . $monthNameText[Date('M', $unixTime)] . date('Y', $unixTime);
+	echo $dayNameText[Date('D', $unixTime)] . ', ' . date('j', $unixTime) . '. ' . $monthNameText[Date('M', $unixTime)] . ' ' . date('Y', $unixTime);
 }
 ?>
 
