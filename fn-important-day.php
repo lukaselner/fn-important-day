c40d9b3622abb90159742834a1c6ae7a1c2f0bec
Lukáš Elner <65008657+lukaselner@users.noreply.github.com>
2020-05-08 09:11:06 +0200

    Add files via upload

---
+++ fn-important-day.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Author: Lukas Elner
+E-mail: lukas@elner.cz
+Web: lukas.elner.cz
+Version: CZECH
+*/
+
+function importantDay($dayD) {
+	$unixTime = strtotime($dayD);
+	$dayNameText = array('Mon' => 'Pondělí', 'Tue' => 'Úterý', 'Wed' => 'Středa', 'Thu' => 'Čtvrtek', 'Fri' => 'Pátek', 'Sat' => 'Sobota', 'Sun' => 'Neděle');
+	$monthNameText = array('Jan' => 'ledna', 'Feb' => 'února', 'Mar' => 'března', 'Apr' => 'dubna', 'May' => 'května', 'Jun' => 'června', 'Jul' => 'července', 'Aug' => 'srpna', 'Sep' => 'září', 'Oct' => 'října', 'Nov' => 'listopadu', 'Dec' => 'prosince');
+	echo $dayNameText[Date('D', $unixTime)] . ', ' . date('j', $unixTime) . '. ' . $monthNameText[Date('M', $unixTime)] . date('Y', $unixTime);
+}
+?>
+
+<?php
+importantDay('2020-01-01');
+?>
