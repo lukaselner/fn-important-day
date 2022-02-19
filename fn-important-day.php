695d655e137242b8bfae592716747cf1c4934ffe
Lukáš Elner <65008657+lukaselner@users.noreply.github.com>
2022-02-19 17:53:50 +0100

    Update fn-important-day.php

--- fn-important-day.php
+++ fn-important-day.php
@@ -1,19 +1,22 @@
 <?php
 /*
+PHP function - Important Day's with translate
+
 Author: Lukas Elner
 E-mail: lukas@elner.cz
 Web: lukas.elner.cz
 Version: CZECH
+Revision: 0.2
 */
 
 function importantDay($dayD) {
 	$unixTime = strtotime($dayD);
 	$dayNameText = array('Mon' => 'Pondělí', 'Tue' => 'Úterý', 'Wed' => 'Středa', 'Thu' => 'Čtvrtek', 'Fri' => 'Pátek', 'Sat' => 'Sobota', 'Sun' => 'Neděle');
 	$monthNameText = array('Jan' => 'ledna', 'Feb' => 'února', 'Mar' => 'března', 'Apr' => 'dubna', 'May' => 'května', 'Jun' => 'června', 'Jul' => 'července', 'Aug' => 'srpna', 'Sep' => 'září', 'Oct' => 'října', 'Nov' => 'listopadu', 'Dec' => 'prosince');
-	echo $dayNameText[Date('D', $unixTime)] . ', ' . date('j', $unixTime) . '. ' . $monthNameText[Date('M', $unixTime)] . ' ' . date('Y', $unixTime);
+	return $dayNameText[Date('D', $unixTime)] . ', ' . date('j', $unixTime) . '. ' . $monthNameText[Date('M', $unixTime)] . ' ' . date('Y', $unixTime);
 }
 ?>
 
 <?php
-importantDay('2020-01-01');
+echo importantDay('2020-01-01');
 ?>
