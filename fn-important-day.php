fdb0ef0535d2fe3d4430a2e06d02c9910e048884
Lukáš Elner <65008657+lukaselner@users.noreply.github.com>
2023-07-31 08:44:17 +0200

    Update fn-important-day.php
    Cosmetic update mini

--- fn-important-day.php
+++ fn-important-day.php
@@ -5,13 +5,13 @@ Author: Lukas Elner
 E-mail: lukas@elner.cz
 Web: lukas.elner.cz
 Version: CZECH
-Revision: 0.3
+Revision: 0.31
 */
 function importantDay($day_d) {
 	$unix_time = strtotime($day_d);
 	$day_name_val = ['Mon' => 'Pondělí', 'Tue' => 'Úterý', 'Wed' => 'Středa', 'Thu' => 'Čtvrtek', 'Fri' => 'Pátek', 'Sat' => 'Sobota', 'Sun' => 'Neděle'];
 	$month_name_val = ['Jan' => 'ledna', 'Feb' => 'února', 'Mar' => 'března', 'Apr' => 'dubna', 'May' => 'května', 'Jun' => 'června', 'Jul' => 'července', 'Aug' => 'srpna', 'Sep' => 'září', 'Oct' => 'října', 'Nov' => 'listopadu', 'Dec' => 'prosince'];
-	return $day_name_val[Date('D', $unix_time)] . ', ' . date('j', $unix_time) . '. ' . $month_name_val[Date('M', $unix_time)] . ' ' . date('Y', $unix_time);
+	return $day_name_val[date('D', $unix_time)] . ', ' . date('j', $unix_time) . '. ' . $month_name_val[date('M', $unix_time)] . ' ' . date('Y', $unix_time);
 }
 echo importantDay('1970-01-01');
 ?>
